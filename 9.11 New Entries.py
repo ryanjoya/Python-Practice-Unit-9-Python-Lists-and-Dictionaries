69b89e6b3379bf95150431b7cda5314af9543a0b
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-03-23 21:17:51 -0500

    Create 9.11 New Entries.py

---
+++ 9.11 New Entries.py
@@ -0,0 +1,11 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+menu['Ultimate Bacon Cheeseburger'] = 6.09
+menu['Chocolate Shake'] = 1.99
+menu['Animal Style Fries'] = 3.40
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
